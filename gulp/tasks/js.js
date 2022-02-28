5fefd10965660af47ee34e7fafe0eb59b9db59e1
vadym maslovskyi <vadim3d2011@gmail.com>
2022-02-28 21:55:16 +0200

    config 2.1.0

---
+++ gulp/tasks/js.js
@@ -0,0 +1,57 @@
+import webpack from "webpack-stream";
+
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+
+  if (preset) {
+    opts.presets.push(preset)
+  }
+
+  return opts
+}
+
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions()
+  }]
+  return loaders
+}
+
+export const js = () => {
+	return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
+		.pipe(app.plugins.plumber(
+			app.plugins.notify.onError({
+				title: "JS",
+				message: "Error: <%= error.message %>"
+			}))
+		)
+		.pipe(webpack({
+			mode: app.isBuild ? 'production' : 'development',
+			entry: {
+				main: ['@babel/polyfill', './src/js/app.js']
+			},
+			output: {
+				filename: 'app.min.js',
+			},
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						use: jsLoaders()
+					}
+				]
+			}
+		}))
+		.pipe(app.gulp.dest(app.path.build.js))
+		.pipe(app.plugins.browsersync.stream());
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,277 +1,59 @@
-let source_folder = 'src';
-// let project_folder = 'E:/OpenServer/domains/localhost/test';
-let project_folder = 'dist';
+// Основной модуль
+import gulp from "gulp";
+// Импорт путей
+import { path } from "./gulp/config/path.js";
+// Импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
 
-let fs = require('fs');
-
-let path = {
-    build: {
-        html: project_folder + '/',
-        css: project_folder + '/css/',
-        js: project_folder + '/js/',
-        img: project_folder + '/img/',
-        icon: project_folder + '/icons/',
-        fonts: project_folder + '/fonts/',
-        php: project_folder + '/',
-        phpbasic: project_folder + '/phpmailer/',
-        db: project_folder + '/',
-    },
-    src: {
-        html: source_folder + '/*.html',
-        css: source_folder + '/scss/style.scss',
-        js: source_folder + '/js/main.js',
-        img: source_folder + '/img/**/*.{png,jpg,svg,gif,ico,webp}',
-        icon: source_folder + '/icons/**/*.{png,jpg,svg,gif,ico,webp}',
-        fonts: source_folder + '/fonts/*.{ttf,woff,woff2}',
-        php: source_folder + '/sendmail.php',
-        phpbasic: source_folder + '/phpmailer/**',
-        db: source_folder + '/*.json',
-    },
-
-    watch: {
-        html: source_folder + '/**/*.html',
-        css: source_folder + '/scss/**/*.scss',
-        js: source_folder + '/js/**/*.js',
-        img: source_folder + '/img/**/*.{png,jpg,svg,gif,ico,webp}',
-        icon: source_folder + '/icons/**/*.{png,jpg,svg,gif,ico,webp}',
-        db: source_folder + '/*.json',
-    },
-
-    clean: './' + project_folder + '/'
+// Передаем значения в глобальную переменную
+global.app = {
+	isBuild: process.argv.includes('--build'),
+	isDev: !process.argv.includes('--build'),
+	path: path,
+	gulp: gulp,
+	plugins: plugins
 }
 
+// Импорт задач
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSpriteTask } from "./gulp/tasks/svg-sprive.js";
+import { zip } from "./gulp/tasks/zip.js";
+import { ftp } from "./gulp/tasks/ftp.js";
 
-let { src, dest } = require('gulp'),
-webpack = require("webpack-stream"),
-gulp = require('gulp'),
-browsersync = require('browser-sync').create(),
-fileinclude = require('gulp-file-include'),
-del = require('del'),
-scss = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-group_media = require('gulp-group-css-media-queries'),
-clean_css = require('gulp-clean-css'),
-rename = require('gulp-rename'),
-imagemin = require('gulp-imagemin'),
-webp = require('gulp-webp'),
-webphtml = require('gulp-webp-html'),
-webpcss = require('gulp-webpcss'),
-ttf2woff = require('gulp-ttf2woff'),
-ttf2woff2 = require('gulp-ttf2woff2'),
-htmlmin = require('gulp-htmlmin'),
-cache = require('gulp-cache');
-
-
-function browserSync(params) {
-    browsersync.init({
-        server: {
-            baseDir: './' + project_folder + '/'
-        },
-        port: 4200,
-        notify: false
-    });
+// Наблюдатель за изменениями в файлах
+function watcher() {
+	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
+	gulp.watch(path.watch.scss, scss);
+	gulp.watch(path.watch.js, js);
+	gulp.watch(path.watch.images, images);
 }
 
+// Последовательная обработака шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
-function html_project() {
-   return src(path.src.html)
-    .pipe(fileinclude())
-    .pipe(webphtml())
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest(path.build.html))
-    .pipe(browsersync.reload({stream: true}));
-}
-function html() {
-    return src(path.src.html)
-        .pipe(fileinclude())
-        .pipe(webphtml())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.reload({stream: true}));
-}
+// Основные задачи
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images, svgSpriteTask));
 
-function css_project() {
-    return src(path.src.css)
-    .pipe(
-        scss({
-            outputStyle: 'compressed'
-        }).on('error', scss.logError)
-    )
-    .pipe(
-        group_media()
-    )
-    .pipe(
-        autoprefixer({
-            grid: true,
-            overrideBrowserslist: ['last 5 versions', '>1%', 'ie 8', 'ie 7'],
-            cascade: true
-        }))
-    .pipe(webpcss())
-    .pipe(clean_css())
-    .pipe(
-        rename({
-            extname: '.min.css'
-        })
-    )
-    .pipe(dest(path.build.css))
-    .pipe(browsersync.reload({stream: true}));
-}
+// Построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
-function css() {
-    return src(path.src.css)
-        .pipe(
-            scss({
-                outputStyle: 'expanded'
-            }).on('error', scss.logError)
-        )
-        .pipe(
-            group_media()
-        )
-        .pipe(
-            autoprefixer({
-                grid: true,
-                overrideBrowserslist: ['last 5 versions', '>1%', 'ie 8', 'ie 7'],
-                cascade: true
-            }))
-        .pipe(webpcss())
-        .pipe(dest(path.build.css))
-        .pipe(browsersync.reload({stream: true}));
-}
+// Экспорт сценариев
+export { svgSpriteTask }
+export { dev }
+export { build }
+export { deployZIP }
+export { deployFTP }
 
-function js() {
-    return gulp.src(path.src.js)
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest(path.build.js))
-    .pipe(browsersync.reload({stream: true}));
-}
-
-function js_project() {
-    return gulp.src(path.src.js)
-                .pipe(webpack(require('./webpack.config.js')))
-                .pipe(gulp.dest(path.build.js));
-}
-
-
-function images() {
-    return src(path.src.img)
-        .pipe(
-            webp({
-                quality: 70,
-                method: 3,
-                autoFilter: true,
-                lossless: true
-            })
-        )
-        .pipe(dest(path.build.img))
-        .pipe(src(path.src.img))
-        .pipe(cache(imagemin({
-                progressive: true,
-                svgoPlugins: [{
-                    removeViewBox: false
-                }],
-                interlaced: true,
-                optimizationLevel: 3
-            })))
-        .pipe(dest(path.build.img))
-        .pipe(browsersync.reload({stream: true}))
-}
-
-gulp.task('clearCache', function() {
-    return cache.clearAll();
-});
-
-
-
-function icons() {
-    return gulp.src(path.src.icon)
-        .pipe(dest(path.build.icon));
-}
-
-function iconsFonts() {
-    return gulp.src('src/fonts/*.svg')
-        .pipe(dest(path.build.fonts));
-}
-
-
-
-function fonts(params) {
-    src(path.src.fonts)
-        .pipe(ttf2woff())
-        .pipe(dest(path.build.fonts));
-    return src(path.src.fonts)
-        .pipe(ttf2woff2())
-        .pipe(dest(path.build.fonts));
-}
-
-
-
-function fontsStyle(params) {
-    let file_content = fs.readFileSync(source_folder + '/scss/basic/fonts.scss');
-    if (file_content == '') {
-        fs.writeFile(source_folder + '/scss/basic/fonts.scss', '', cb);
-        return fs.readdir(path.build.fonts, function (err, items) {
-            if (items) {
-                let c_fontname;
-                for (let i = 0; i < items.length; i++) {
-                    let fontname = items[i].split('.');
-                    fontname = fontname[0];
-                    if (c_fontname != fontname) {
-                        fs.appendFile(source_folder + '/scss/basic/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-                    }
-                    c_fontname = fontname;
-                }
-            }
-        })
-    }
-}
-
-function cb() {
-
-}
-
-function watchFile() {
-    gulp.watch(path.watch.html, html);
-    gulp.watch(path.watch.css, css);
-    gulp.watch(path.watch.js, js);
-    gulp.watch(path.watch.img, images);
-    gulp.watch(path.watch.icon, icons);
-}
-
-
-function clean(params) {
-    return del(path.clean);
-}
-
-
-function php_mailer(){
-    gulp.src(path.src.php)
-     .pipe(dest(path.build.php));
-    return gulp.src(path.src.phpbasic)
-     .pipe(dest(path.build.phpbasic));
-}
-
-function db(){
-    return gulp.src(path.src.db)
-            .pipe(dest(path.build.db));
-}
-
-
-let dev = gulp.series(clean, gulp.parallel(html, css, images, fonts, icons, iconsFonts, php_mailer, db), js, fontsStyle);
-let project = gulp.series(clean, gulp.parallel(html_project, css_project, images, fonts, icons, iconsFonts, php_mailer, db), js_project, fontsStyle);
-
-gulp.task('default', gulp.parallel(dev, watchFile, browserSync));
-gulp.task('prod', gulp.parallel(project, browserSync));
-
-
-exports.html_project = html_project;
-exports.iconsFonts = iconsFonts;
-exports.css_project = css_project;
-exports.js_project = js_project;
-exports.icons = icons;
-exports.fontsStyle = fontsStyle;
-exports.fonts = fonts;
-exports.images = images;
-exports.js = js;
-exports.css = css;
-exports.html = html;
-exports.php_mailer = php_mailer;
-exports.db = db;
+// Выполнение сценария по умолчанию
+gulp.task('default', dev);
